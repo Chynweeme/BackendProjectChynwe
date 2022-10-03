c18d1e6ce207b6f71a51772c0fd4d93ec33f39ed
Adedipupo <ajokanola@terragonltd.com>
2022-10-03 07:58:54 +0100

    wip

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        mongoose.connect(process.env.MONGODB_URL)
         console.log(`Connected to MongoDB Successfully Chynwe ooo`);
     } catch (error) {
         console.error(`Error: ${error.message}`)
